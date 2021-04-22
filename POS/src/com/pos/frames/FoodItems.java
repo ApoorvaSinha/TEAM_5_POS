34f4bda2509dc6a4a190acec353708e7391575e4
Anubhav Jain <anubhav170499@gmail.com>
2021-04-22 12:19:12 +0530

    FoodItems Frame

--- POS/src/com/pos/frames/FoodItems.java
+++ POS/src/com/pos/frames/FoodItems.java
@@ -2,26 +2,50 @@ package com.pos.frames;
 
 
 import java.awt.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.swing.*;
+
+import com.pos.dao.AdminDAO;
+import com.pos.model.Food;
 
 public class FoodItems {
 	JFrame frame;
 	JPanel panel;
 	
 	
-	public FoodItems()
+	public void setFoodItems() throws SQLException
 	{   
-		frame=new JFrame("Food Items");    
-	    String data[][]={ 
-		    		
-		    		{"Double Cheese", "Veg", "Small", "500", "500"},    
-		    		{"Triple Cheese", "Non-Veg", "Medium", "400", "400"},    
-		    		{"Quadruple Cheese", "Veg", "Large", "600", "600"}
-	    		};    
+		frame=new JFrame("Food Items");  
+		AdminDAO ad = new AdminDAO();
+		
+		String[][] data;
+		
+		ArrayList<Food> foodList = ad.getAllFoodItems();
+		
+		data = new String[foodList.size()][6];
+		
+		for(int i=0; i<foodList.size(); i++)
+		{
+			data[i][0] = foodList.get(i).getFoodId();
+			data[i][1] = foodList.get(i).getName();
+			data[i][2] = foodList.get(i).getType();
+			data[i][3] = foodList.get(i).getFoodSize();
+			data[i][4] = String.valueOf(foodList.get(i).getQuantity());
+			data[i][5] = String.valueOf(foodList.get(i).getPrice());
+		}
+		
+//	    String data[][]={ 
+//		    		
+//		    		{"Double Cheese", "Veg", "Small", "500", "500"},    
+//		    		{"Triple Cheese", "Non-Veg", "Medium", "400", "400"},    
+//		    		{"Quadruple Cheese", "Veg", "Large", "600", "600"}
+//	    		};    
 	    
-	    String column[]={"Name", "Type", "Size", "Qty", "Price"};
+	    String column[]={"FoodId", "Name", "Type", "Size", "Qty", "Price"};
 	    
-	    JTable jt=new JTable(data,column);    
+	    JTable jt= new JTable(data, column);    
 	    jt.setBounds(100,100,200,300); 
 	    
 	    
@@ -42,7 +66,7 @@ public class FoodItems {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
-	public static void main(String[] args) {
-		new FoodItems();
+	public static void main(String[] args) throws SQLException {
+		new FoodItems().setFoodItems();
 	}
 }
