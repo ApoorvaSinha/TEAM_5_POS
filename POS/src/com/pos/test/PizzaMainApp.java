8dc7dd8304bb385c0b8c3575f6a14e54a7e4e94f
Aks647 <akshat.k.647@gmail.com>
2021-04-26 12:48:13 +0530

    Add

---
+++ POS/src/com/pos/test/PizzaMainApp.java
@@ -0,0 +1,25 @@
+package com.pos.test;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import com.pos.dao.AdminDAO;
+import com.pos.frames.Login;
+import com.pos.frames.PaymentGateway;
+
+public class PizzaMainApp {
+
+	public static void main(String[] args) {
+		AdminDAO ad = new AdminDAO();
+		try {
+
+			new Login();
+
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
